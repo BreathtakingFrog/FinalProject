06debf38d40282473c2b517e61f8fbcd7795424f
Yeldos-cyberbully <80340017+Yeldos-cyberbully@users.noreply.github.com>
2021-03-12 10:22:53 +0600

    Update BaseConnect.java

--- final_project/BaseConnect.java
+++ final_project/BaseConnect.java
@@ -3,8 +3,8 @@ package kz.aitu.oop.final_project;
 
 import java.sql.Connection;
 
-public interface BaseConnect {
+public interface BaseConnect { // it is for connect to the database
 
-    public Connection connect(String url, String user, String password);
-    public String execSQL(String sql,String [] params);
+    public Connection connect(String url, String user, String password); // connect with url, user and password
+    public String execSQL(String sql,String [] params); 
 }
